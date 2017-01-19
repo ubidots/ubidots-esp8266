8934d52fe30df55d628a55b8d7c1223c78a24981
mariacarlinahernandez <maria@ubidots.com>
2017-01-19 16:33:49 -0500

    Add files via upload
    Examples added.

---
+++ Examples/UbidotsGetVarContext/UbidotsGetVarContext.ino
@@ -0,0 +1,15 @@
+#include "UbidotsMicroESP8266.h"
+#define ID  "Your_VariableID_here"  // Put here your Ubidots variable ID
+#define TOKEN  "Your_token_here"  // Put here your Ubidots TOKEN
+#define WIFISSID "Your_WiFi_SSID"
+#define PASSWORD "Your_WiFi_Password"
+
+Ubidots client(TOKEN);
+
+void setup() {
+    Serial.begin(115200);
+    client.wifiConnection(WIFISSID, PASSWORD);
+}
+void loop() {
+    char* context = client.getVarContext(ID);
+}
