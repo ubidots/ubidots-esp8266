edda5b56dd216c9b130c185060261e6123671716
Jose Garcia <reyes2000jose@gmail.com>
2019-10-20 18:55:33 -0500

    updating examples

---
+++ Examples/GetValueHttp/GetValueHttp.cpp
@@ -0,0 +1,43 @@
+// This example retrieves last value of a variable from the Ubidots API
+// using HTTP protocol.
+
+/****************************************
+ * Include Libraries
+ ****************************************/
+
+#include "Ubidots.h"
+
+/****************************************
+ * Define Constants
+ ****************************************/
+
+const char* UBIDOTS_TOKEN = "...";  // Put here your Ubidots TOKEN
+
+Ubidots ubidots(UBIDOTS_TOKEN, UBI_HTTP);
+
+/****************************************
+ * Auxiliar Functions
+ ****************************************/
+
+// Put here your auxiliar functions
+
+/****************************************
+ * Main Functions
+ ****************************************/
+
+void setup() {
+  Serial.begin(115200);
+  // ubidots.setDebug(true); //Uncomment this line for printing debug messages
+}
+
+void loop() {
+  /* Obtain last value from a variable as float using HTTP */
+  float value = ubidots.get("weather-station", "temperature");
+
+  // Evaluates the results obtained
+  if (value != ERROR_VALUE) {
+    Serial.print("Value:");
+    Serial.println(value);
+  }
+  delay(5000);
+}
---
+++ Examples/GetValueTcp/GetValueTcp.cpp
@@ -0,0 +1,41 @@
+// This example retrieves last value of a variable from the Ubidots API
+// using TCP protocol.
+
+/****************************************
+ * Include Libraries
+ ****************************************/
+
+#include "Ubidots.h"
+
+/****************************************
+ * Define Constants
+ ****************************************/
+
+const char* UBIDOTS_TOKEN = "...";  // Put here your Ubidots TOKEN
+Ubidots ubidots(UBIDOTS_TOKEN, UBI_TCP);
+
+/****************************************
+ * Auxiliar Functions
+ ****************************************/
+
+// Put here your auxiliar functions
+
+/****************************************
+ * Main Functions
+ ****************************************/
+
+void setup() {
+  Serial.begin(115200);
+  // ubidots.setDebug(true); //Uncomment this line for printing debug messages
+}
+void loop() {
+  /* Obtain last value from a variable as float using TCP */
+  float value = ubidots.get("weather-station", "temperature");
+
+  // Evaluates the results obtained
+  if (value != ERROR_VALUE) {
+    Serial.print("Value:");
+    Serial.println(value);
+  }
+  delay(5000);
+}
